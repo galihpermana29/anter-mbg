f6264827ad0d8f75fd337617639f34488ce92828
galihpermana29 <galihcbn123@gmail.com>
2025-09-01 22:39:23 +0700

    push back blank route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,21 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { getSessionClient } from "@/shared/session/get-session-client";
+
 const Page = () => {
+  const router = useRouter();
+
+  const checkSession = async () => {
+    const data = await getSessionClient();
+    if (!data || !data.access_token) {
+      router.push("/login");
+    }
+  };
+  useEffect(() => {
+    checkSession();
+  }, []);
   return <div></div>;
 };
 
